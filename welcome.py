b7805a2010098900af4a710a8a08828754c290bb
Jie Xu <107989499+jiexu2776@users.noreply.github.com>
2023-08-03 12:43:27 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -11,7 +11,7 @@ for i in os.listdir():
     file_name_list.append(i)
 
 
-select_file = st.selectbox('select location', file_name_list)
+
 
 st.write('Hello world')
 
@@ -27,38 +27,43 @@ y_data = st.selectbox('select y element', el_list)
 
 datafiles = st.multiselect('select location', file_name_list, file_name_list[0])
 
+col1, col2 = st.columns(2)
 
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 2, 4, 5]
-
-p = figure(
-    title='simple line example',
-    x_axis_label=x_data,
-    y_axis_label=y_data)
-
-genre = st.radio(
-    "What\'s your factor for standard deviation",
-    ('1sd', '2sd', '3sd'))
-colors = ['red', 'navy', 'black', 'pink', 'purple']
-for i, color in zip(datafiles, colors):
-  df = pd.read_csv(i)
-  p.circle(df[x_data], df[y_data], color=color, size = 15, legend_label=i[:-4], line_width=2)
-  p.line(df[x_data], df[y_data].mean(), color=color, line_width=2)
+with col1:
+  ele = st.selectbox('element to display', el_list)
+  for i in datafiles:
+    df = pd.read_csv(i)
+    st.dataframe(df[ele])
+    
+with col2:
+  p = figure(
+      title='simple line example',
+      x_axis_label=x_data,
+      y_axis_label=y_data)
   
-  if genre == '1sd':
-    p.line(df[x_data], df[y_data].mean()-df[y_data].std(),color=color, line_width=2)
-    p.line(df[x_data], df[y_data].mean()+df[y_data].std(), color=color,line_width=2)
-    st.write('You selected 1sd')
-  if genre == '2sd':
-    p.line(df[x_data], df[y_data].mean()-2*df[y_data].std(), color=color,line_width=2)
-    p.line(df[x_data], df[y_data].mean()+2*df[y_data].std(), color=color,line_width=2)
-    st.write('You selected 2sd')
-  if genre == '3sd':
-    p.line(df[x_data], df[y_data].mean()-3*df[y_data].std(), color=color,line_width=2)
-    p.line(df[x_data], df[y_data].mean()+3*df[y_data].std(), color=color,line_width=2)
-    st.write('You selected 3sd')  
+  genre = st.radio(
+      "What\'s your factor for standard deviation",
+      ('1sd', '2sd', '3sd'))
+  colors = ['red', 'navy', 'black', 'pink', 'purple']
+  for i, color in zip(datafiles, colors):
+    df = pd.read_csv(i)
+    p.circle(df[x_data], df[y_data], color=color, size = 15, legend_label=i[:-4], line_width=2)
+    p.line(df[x_data], df[y_data].mean(), color=color, line_width=2)
+    
+    if genre == '1sd':
+      p.line(df[x_data], df[y_data].mean()-df[y_data].std(),color=color, line_width=2)
+      p.line(df[x_data], df[y_data].mean()+df[y_data].std(), color=color,line_width=2)
+      st.write('You selected 1sd')
+    if genre == '2sd':
+      p.line(df[x_data], df[y_data].mean()-2*df[y_data].std(), color=color,line_width=2)
+      p.line(df[x_data], df[y_data].mean()+2*df[y_data].std(), color=color,line_width=2)
+      st.write('You selected 2sd')
+    if genre == '3sd':
+      p.line(df[x_data], df[y_data].mean()-3*df[y_data].std(), color=color,line_width=2)
+      p.line(df[x_data], df[y_data].mean()+3*df[y_data].std(), color=color,line_width=2)
+      st.write('You selected 3sd')  
   
+  st.bokeh_chart(p, use_container_width=True)
 
 
 
-st.bokeh_chart(p, use_container_width=True)
