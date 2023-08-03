d59261085cd69fcb9f25b1e5f68cb48347b56d8a
Jie Xu <107989499+jiexu2776@users.noreply.github.com>
2023-08-03 12:13:35 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -37,5 +37,8 @@ p = figure(
     y_axis_label=y_data)
 
 p.line(df[x_data], df[y_data], legend_label='Trend', line_width=2)
+p.line(df[x_data], df[y_data].mean(), line_width=2)
+
+
 
 st.bokeh_chart(p, use_container_width=True)
