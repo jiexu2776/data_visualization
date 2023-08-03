9a4dbdfe3abf9c298e5b4a9754573357fcb66fab
Jie Xu <107989499+jiexu2776@users.noreply.github.com>
2023-08-03 12:20:41 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -36,10 +36,29 @@ p = figure(
     x_axis_label=x_data,
     y_axis_label=y_data)
 
+
+genre = st.radio(
+    "What\'s your factor for standard deviation",
+    ('1sd', '2sd', '3sd'))
+
+if genre == '1sd':
+  p.line(df[x_data], df[y_data].mean()-df[y_data].std(), line_width=2)
+  p.line(df[x_data], df[y_data].mean()+df[y_data].std(), line_width=2)
+  st.write('You selected 1sd')
+if genre == '2sd':
+  p.line(df[x_data], df[y_data].mean()-2*df[y_data].std(), line_width=2)
+  p.line(df[x_data], df[y_data].mean()+2*df[y_data].std(), line_width=2)
+  st.write('You selected 2sd')
+else:
+  p.line(df[x_data], df[y_data].mean()-3*df[y_data].std(), line_width=2)
+  p.line(df[x_data], df[y_data].mean()+3*df[y_data].std(), line_width=2)
+  st.write('You selected 3sd')  
+
+
+
 p.circle(df[x_data], df[y_data], color="navy", size = 15, legend_label='Trend', line_width=2)
 p.line(df[x_data], df[y_data].mean(), line_width=2)
-p.line(df[x_data], df[y_data].mean()-df[y_data].std(), line_width=2)
-p.line(df[x_data], df[y_data].mean()+df[y_data].std(), line_width=2)
+
 
 
 st.bokeh_chart(p, use_container_width=True)
