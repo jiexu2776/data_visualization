09c2d458629c17282a35458d9527abd0831e8df2
Jie Xu <107989499+jiexu2776@users.noreply.github.com>
2023-08-03 12:26:23 +0200

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -25,7 +25,7 @@ y_data = st.selectbox('select y element', el_list)
 
 
 
-st.multiselect('select location', file_name_list, file_name_list[0])
+datafiles = st.multiselect('select location', file_name_list, file_name_list[0])
 
 
 x = [1, 2, 3, 4, 5]
@@ -36,27 +36,28 @@ p = figure(
     x_axis_label=x_data,
     y_axis_label=y_data)
 
-
-p.circle(df[x_data], df[y_data], color="navy", size = 15, legend_label='Trend', line_width=2)
-p.line(df[x_data], df[y_data].mean(), color="pink", line_width=2)
-
 genre = st.radio(
     "What\'s your factor for standard deviation",
     ('1sd', '2sd', '3sd'))
 
-if genre == '1sd':
-  p.line(df[x_data], df[y_data].mean()-df[y_data].std(), line_width=2)
-  p.line(df[x_data], df[y_data].mean()+df[y_data].std(), line_width=2)
-  st.write('You selected 1sd')
-if genre == '2sd':
-  p.line(df[x_data], df[y_data].mean()-2*df[y_data].std(), line_width=2)
-  p.line(df[x_data], df[y_data].mean()+2*df[y_data].std(), line_width=2)
-  st.write('You selected 2sd')
-if genre == '3sd':
-  p.line(df[x_data], df[y_data].mean()-3*df[y_data].std(), line_width=2)
-  p.line(df[x_data], df[y_data].mean()+3*df[y_data].std(), line_width=2)
-  st.write('You selected 3sd')  
-
+for i in datafiles:
+  
+  p.circle(i[x_data], i[y_data], color="navy", size = 15, legend_label='Trend', line_width=2)
+  p.line(i[x_data], i[y_data].mean(), color="pink", line_width=2)
+  
+  if genre == '1sd':
+    p.line(i[x_data], i[y_data].mean()-i[y_data].std(), line_width=2)
+    p.line(i[x_data], i[y_data].mean()+i[y_data].std(), line_width=2)
+    st.write('You selected 1sd')
+  if genre == '2sd':
+    p.line(i[x_data], i[y_data].mean()-2*i[y_data].std(), line_width=2)
+    p.line(i[x_data], i[y_data].mean()+2*i[y_data].std(), line_width=2)
+    st.write('You selected 2sd')
+  if genre == '3sd':
+    p.line(i[x_data], i[y_data].mean()-3*i[y_data].std(), line_width=2)
+    p.line(i[x_data], i[y_data].mean()+3*i[y_data].std(), line_width=2)
+    st.write('You selected 3sd')  
+  
 
 
 
